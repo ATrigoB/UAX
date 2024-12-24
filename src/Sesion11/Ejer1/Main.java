08a91cc38bafa6fb49a0bf803de5175658df4655
Adrián Trigo Bravo <adriantrigobravo@gmail.com>
2024-12-24 20:59:35 +0100

    Añadir Ejercicio 1 resuelto y fichero enunciado

--- src/Sesion11/Ejer1/Main.java
+++ src/Sesion11/Ejer1/Main.java
@@ -3,10 +3,24 @@ package Sesion11.Ejer1;
 /*
  */
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
 
+        int [] numeros = new int[10];
 
+        System.out.println("Introduce 10 numeros por teclado: ");
+        Scanner teclado = new Scanner(System.in);
+        ArrayList <Integer> inversos = new ArrayList<>();
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = teclado.nextInt();
+
+            inversos.add(0,numeros[i]);
+        }
+        System.out.println(Arrays.toString(numeros));
+        System.out.println(inversos);
     }
 }
